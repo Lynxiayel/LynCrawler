d0add5fa055733448cf9f950107a6842f3bc7050
lynxiayel <lxytcmn@gmail.com>
2014-10-13 14:42:47 -0400

    updated PhoneSpecsCrawler.py to correctly import LynCrawler

--- examples/PhoneSpecsCrawler.py
+++ examples/PhoneSpecsCrawler.py
@@ -1,4 +1,8 @@
 __author__ = 'lynxiayel'
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
 from LynCrawler import LynCrawler
 import re
 
