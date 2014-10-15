e2b7fd0a67e42cc70d5026b1bc50c92ec30bbd3f
lynxiayel <lxytcmn@gmail.com>
2014-10-14 20:38:54 -0400

    new phone_specs crawled; modified PhoneData.py to extract stand-by info

--- examples/PhoneData.py
+++ examples/PhoneData.py
@@ -12,12 +12,15 @@ class PhoneData:
         monthDict = dict({'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12})
         months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
         monthPat = re.compile('(' + '|'.join(months) + ')', re.IGNORECASE)
-        yearPat = re.compile(r'(\d+)')
+        yearPat = re.compile(r'(\d+)$')
         result=[]
         for i in self.data:
             date = i['announceDate'][0].split(', ')
             if len(date) >= 2 :
-                year = yearPat.findall(date[0])
+                if 'Q' in date[0]:#incase of date string like'1Q, 2003'
+                    year = yearPat.findall(date[1])
+                else:
+                    year = yearPat.findall(date[0])
                 if year:
                     year=year[0]
                 else:
@@ -80,18 +83,45 @@ class PhoneData:
         return brand
 
     def getBattery(self):
-        batPat = re.compile(r'(\d+?) mAh', re.IGNORECASE)
+        batPat = re.compile(r'([\d,]+?) mAh', re.IGNORECASE)
         battery=[]
         for i in self.data:
             if i['battery']:
                 tmp=batPat.findall(i['battery'][0])
                 if tmp:
-                    battery.append(int(tmp[0]))
+                    battery.append(int(tmp[0].replace(',','')))
                 else:
                     battery.append(0)
             else:
                 battery.append(0)
         return battery
+
+    def getStandBy(self):
+        sdb=[]
+        for i in self.data:
+            if i['stand-by']:
+                sdb.append(int(i['stand-by'][0]))
+            else:
+                sdb.append(0)
+        return sdb
+
+    def getStandBy2G(self):
+        sdb=[]
+        for i in self.data:
+            if i['stand-by-2g']:
+                sdb.append(int(i['stand-by-2g'][0]))
+            else:
+                sdb.append(0)
+        return sdb
+
+    def getStandBy3G(self):
+        sdb=[]
+        for i in self.data:
+            if i['stand-by-3g']:
+                sdb.append(int(i['stand-by-3g'][0]))
+            else:
+                sdb.append(0)
+        return sdb
 
 if __name__=='__main__':
     data=PhoneData()
@@ -100,31 +130,7 @@ if __name__=='__main__':
     date=data.getDate()
     name=data.getName()
     size=data.getSize()
-    print len(name),len(date),len(size), len(brand),len(bat)
-#with open('phone_specs.txt', 'r') as f:
-#    s = []
-#    for i in f.readlines():
-#        s.append(dict(ast.literal_eval(i)))
-#months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
-#monthPat = re.compile('(' + '|'.join(months) + ')', re.IGNORECASE)
-#yearPat = re.compile(r'(\d+)')
-#batPat = re.compile(r'(\d+?) mAh', re.IGNORECASE)
-#brandPat = re.compile(r'(\w+?) ')
-#sizePat = re.compile(r'([\d.]+?) inches')
-#with open('battery.txt', 'w') as b:
-#    for i in s:
-#        date = i['announceDate'][0].split(', ')
-#        bat = i['battery']
-#        name = i['name']
-#        stand_by=i['stand-by']
-#        stand_by_2g=i['stand-by-2g']
-#        stand_by_3g=i['stand-by-3g']
-#        size = i['size']
-#        if len(date) >= 2 and len(bat) >= 1:
-#            year = yearPat.findall(date[0])
-#            month = monthPat.findall(date[1])
-#            bat = batPat.findall(bat[0])
-#            brand = brandPat.findall(name[0])
-#            size = sizePat.findall(size[0])
-#            if month and year and bat and brand and size:
-#                b.write(name[0]+' '+year[0] + '.' + str(monthDict[month[0]]) + ' ' + bat[0] + ' ' + size[0] + ' ' + brand[0] + '\n')
+    sdb=data.getStandBy()
+    sdb2=data.getStandBy2G()
+    sdb3=data.getStandBy3G()
+    print zip(name,sdb,sdb2, sdb3)
