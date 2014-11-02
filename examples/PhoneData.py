e5659c037eb33aa9ba840bd0abc9c8968b613dcc
lynxiayel <lxytcmn@gmail.com>
2014-11-02 00:58:36 -0400

    refine the data expression; add plotting of stand-by time

--- examples/PhoneData.py
+++ examples/PhoneData.py
@@ -1,52 +1,78 @@
 import re
 import ast
+from datetime import datetime
+
 
 class PhoneData:
-    def __init__(self,file='phone_specs.txt'):
-        self.data=[]
+
+    def __init__(self, file='phone_specs.txt'):
+        self.data = []
         with open(file, 'r') as f:
             for i in f.readlines():
-                self.data.append(dict(ast.literal_eval(i)))
+                newitem=dict(ast.literal_eval(i))
+                tmp=0
+                for v in newitem.values():
+                    if v:
+                        tmp+=1
+                if tmp>1:
+                    self.data.append(newitem)
 
     def getDate(self):
-        monthDict = dict({'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12})
-        months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+        monthDict = dict({'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5,
+                          'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12})
+        months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun',
+                  'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
         monthPat = re.compile('(' + '|'.join(months) + ')', re.IGNORECASE)
+        quarterPat=re.compile('(\d)Q')
         yearPat = re.compile(r'(\d+)$')
-        result=[]
+        MONTH_RATIO=float(1)/12
+        result = []
         for i in self.data:
-            date = i['announceDate'][0].split(', ')
-            if len(date) >= 2 :
-                if 'Q' in date[0]:#incase of date string like'1Q, 2003'
-                    year = yearPat.findall(date[1])
-                else:
+            if i['announceDate']:
+                date = i['announceDate'][0].split(', ')
+                if len(date) >= 2:
+                    if 'Q' in date[0]:  # incase of date string like'1Q, 2003'
+                        year = yearPat.findall(date[1])
+                        quarter=quarterPat.findall(date[0])
+                        if quarter:
+                            month=int(quarter[0])*3-2
+                            month=(month-1)*MONTH_RATIO
+                        else:
+                            month=0
+                    else:
+                        year = yearPat.findall(date[0])
+                        month = monthPat.findall(date[1])
+                        if month:
+                            month = int(str(monthDict[month[0]]))
+                            month=(month-1)*MONTH_RATIO
+                        else:
+                            month =0
+                    if year:
+                        year = int(year[0])
+                    else:
+                        year = 0
+                elif len(date) == 1:
+                    month = 0
                     year = yearPat.findall(date[0])
-                if year:
-                    year=year[0]
+                    if year:
+                        year = int(year[0])
+                    else:
+                        year = 0
                 else:
-                    year='0'
-                month = monthPat.findall(date[1])
-                if month:
-                    month=str(monthDict[month[0]])
-                else:
-                    month='0'
-            elif len(date)==1:
-                month='0'
-                year = yearPat.findall(date[0])
-                if year:
-                    year=year[0]
-                else:
-                    year='0'
+                    year = 0
+                    month = 0
             else:
-                year='0'
-                month='0'
-            tmp=year+'.'+month
-            result.append(float(tmp))
+                year = 0
+                month = 0
+            if year==0:
+                date=0
+            else:
+                date=year+month
+            result.append(date)
         return result
 
-
     def getName(self):
-        name=[]
+        name = []
         for i in self.data:
             if i['name']:
                 name.append(i['name'][0])
@@ -56,10 +82,10 @@ class PhoneData:
 
     def getSize(self):
         sizePat = re.compile(r'([\d.]+?) inches')
-        size=[]
+        size = []
         for i in self.data:
             if i['size']:
-                tmp=sizePat.findall(i['size'][0])
+                tmp = sizePat.findall(i['size'][0])
                 if tmp:
                     size.append(float(tmp[0]))
                 else:
@@ -70,10 +96,10 @@ class PhoneData:
 
     def getBrand(self):
         brandPat = re.compile(r'(\w+?) ')
-        brand=[]
+        brand = []
         for i in self.data:
             if i['name']:
-                tmp=brandPat.findall(i['name'][0])
+                tmp = brandPat.findall(i['name'][0])
                 if tmp:
                     brand.append(tmp[0])
                 else:
@@ -84,12 +110,12 @@ class PhoneData:
 
     def getBattery(self):
         batPat = re.compile(r'([\d,]+?) mAh', re.IGNORECASE)
-        battery=[]
+        battery = []
         for i in self.data:
             if i['battery']:
-                tmp=batPat.findall(i['battery'][0])
+                tmp = batPat.findall(i['battery'][0])
                 if tmp:
-                    battery.append(int(tmp[0].replace(',','')))
+                    battery.append(int(tmp[0].replace(',', '')))
                 else:
                     battery.append(0)
             else:
@@ -97,7 +123,7 @@ class PhoneData:
         return battery
 
     def getStandBy(self):
-        sdb=[]
+        sdb = []
         for i in self.data:
             if i['stand-by']:
                 sdb.append(int(i['stand-by'][0]))
@@ -106,7 +132,7 @@ class PhoneData:
         return sdb
 
     def getStandBy2G(self):
-        sdb=[]
+        sdb = []
         for i in self.data:
             if i['stand-by-2g']:
                 sdb.append(int(i['stand-by-2g'][0]))
@@ -115,7 +141,7 @@ class PhoneData:
         return sdb
 
     def getStandBy3G(self):
-        sdb=[]
+        sdb = []
         for i in self.data:
             if i['stand-by-3g']:
                 sdb.append(int(i['stand-by-3g'][0]))
@@ -123,14 +149,18 @@ class PhoneData:
                 sdb.append(0)
         return sdb
 
-if __name__=='__main__':
-    data=PhoneData()
-    bat=data.getBattery()
-    brand=data.getBrand()
-    date=data.getDate()
-    name=data.getName()
-    size=data.getSize()
-    sdb=data.getStandBy()
-    sdb2=data.getStandBy2G()
-    sdb3=data.getStandBy3G()
-    print zip(name,sdb,sdb2, sdb3)
+    def getCount(self):
+        return len(self.data)
+
+if __name__ == '__main__':
+    data = PhoneData()
+    bat = data.getBattery()
+    brand = data.getBrand()
+    date = data.getDate()
+    name = data.getName()
+    size = data.getSize()
+    sdb = data.getStandBy()
+    sdb2 = data.getStandBy2G()
+    sdb3 = data.getStandBy3G()
+    #print zip(name, sdb, sdb2, sdb3)
+    print data.getCount()
--- examples/drawBattery.py
+++ examples/drawBattery.py
@@ -13,30 +13,32 @@ def sortCorrelatedData(*args):
     '''sort correlated data given in list1, list2,...
         list1 is used as the key for sorting defaultly
     '''
-    tups=zip(*args)
+    tups = zip(*args)
     tups.sort()
     return zip(*tups)
 
-def excludeTablet(index,size,*args):
+
+def excludeTablet(index, size, *args):
     '''exclude tablet(screenSize>size)
         index denotes the position of the size-list in *args
     '''
-    tups=zip(*args)
-    idxToDel=[]
-    for i,v in enumerate(tups):
-        if v[index]>size:
+    tups = zip(*args)
+    idxToDel = []
+    for i, v in enumerate(tups):
+        if v[index] > size:
             idxToDel.append(i)
     idxToDel.sort(reverse=True)
     for i in idxToDel:
         tups.pop(i)
     return zip(*tups)
 
+
 def excludeEmptyEntry(*args):
-    tups=zip(*args)
-    idxToDel=[]
-    for i,v in enumerate(tups):
+    tups = zip(*args)
+    idxToDel = []
+    for i, v in enumerate(tups):
         for j in v:
-            if j==0 or j=='':
+            if j == 0 or j == '' or j==None:
                 idxToDel.append(i)
                 break
     idxToDel.sort(reverse=True)
@@ -44,44 +46,98 @@ def excludeEmptyEntry(*args):
         tups.pop(i)
     return zip(*tups)
 
-def extractByBrand(index,brand,*args):
+
+def extractByBrand(index, brand, *args):
     '''extract only the data of a specific brand
         index indicates the position of the brand-list in *args
     '''
-    brand=brand.lower()
-    tups=zip(*args)
-    hitIdx=[]
-    for i,v in enumerate(tups):
-        if v[index].lower()==brand:
+    brand = brand.lower()
+    tups = zip(*args)
+    hitIdx = []
+    for i, v in enumerate(tups):
+        if v[index].lower() == brand:
             hitIdx.append(i)
     hitIdx.sort(reverse=True)
-    result=[]
+    result = []
     for i in hitIdx:
         result.append(tups.pop(i))
     return zip(*result)
 
 
+if __name__ == '__main__':
+    data = PhoneData()
+    date = data.getDate()
+    bat = data.getBattery()
+    size = data.getSize()
+    brand = data.getBrand()
+    name = data.getName()
+    sdb = data.getStandBy()
+    sdb2 = data.getStandBy2G()
+    sdb3 = data.getStandBy3G()
+    # the following loop is to remove incorrectly crawled stand-by time, no
+    # need in future refined version
+    for i, v in enumerate(sdb2):
+        if v < 40:
+            sdb2[i] = 0
 
+    # date,size,brand,name,bat,sdb2=extractByBrand(2,'SamSung',date,size,brand,name,bat,sdb2)
+    # plot date-battery
+    date, bat, size = excludeTablet(2, 6.9, date, bat, size)
+    date, bat = excludeEmptyEntry(date, bat)
+    date, bat = sortCorrelatedData(date, bat)
+    print "number of devices in graph", len(date)
+    plt.scatter(date, bat, s=22, alpha=0.5)
+    plt.xlabel('Announced date of the device', fontsize='16')
+    plt.ylabel('Battery compacity (mAh)', fontsize='16')
+    ax = plt.gca()
+    fmt = mpl.ticker.ScalarFormatter(useOffset=False)
+    fmt.set_scientific(False)
+    ax.xaxis.set_major_formatter(fmt)
+    a, b, c, d, e = np.polyfit(date, bat, 4)
+    date = np.array(date)
+    plt.plot(date, a * date ** 4 + b * date **
+             3 + c * date ** 2 + d * date + e, color='g')
+    fig = plt.gcf()
+    fig.set_size_inches(12, 8)
+    fig.savefig('date-battery', dpi=300)
+    plt.close()
 
-
-if __name__=='__main__':
-    data=PhoneData()
-    date=data.getDate()
-    bat=data.getBattery()
-    size=data.getSize()
-    brand=data.getBrand()
-    name=data.getName()
-    sdb2=data.getStandBy2G()
-    #the following loop is to remove incorrectly crawled stand-by time, no need in future refined version
-    for i,v in enumerate(sdb2):
-        if v<40:
-           sdb2[i]=0
-
-    date,size,brand,name,bat,sdb2=excludeEmptyEntry(date,size,brand,name,bat,sdb2)
-    date,size,brand,name,bat,sdb2=sortCorrelatedData(date,size,brand,name,bat,sdb2)
-    date,size,brand,name,bat,sdb2=excludeTablet(1,6.9,date,size,brand,name,bat,sdb2)
-    #date,size,brand,name,bat,sdb2=extractByBrand(2,'SamSung',date,size,brand,name,bat,sdb2)
-    print zip(date,name,sdb2)
-    plt.plot(date,bat)
-    plt.plot(date,sdb2)
-    plt.show()
+    # plot date-stand-by
+    date1, sdb, size = excludeTablet(2, 6.9, date, sdb, size)
+    date1, sdb = excludeEmptyEntry(date1, sdb)
+    date1, sdb = sortCorrelatedData(date1, sdb)
+    date2, sdb2, size = excludeTablet(2, 6.9, date, sdb2, size)
+    date2, sdb2 = excludeEmptyEntry(date2, sdb2)
+    date2, sdb2 = sortCorrelatedData(date2, sdb2)
+    date3, sdb3, size = excludeTablet(2, 6.9, date, sdb3, size)
+    date3, sdb3 = excludeEmptyEntry(date3, sdb3)
+    date3, sdb3 = sortCorrelatedData(date3, sdb3)
+    print "number of devices in graph", len(date)
+    plt.scatter(date1, sdb, s=22, linewidth=1, color='g',marker='+',label='stand-by')
+    plt.scatter(date2, sdb2, s=22,color='b',marker='o',label='stand-by-2g')
+    plt.scatter(date3, sdb3, s=22,linewidth=1, color='k',marker='x',label='stand-by-3g')
+    plt.xlabel('Announced date of the device', fontsize='16')
+    plt.ylabel('Stand-by time (h)', fontsize='16')
+    ax = plt.gca()
+    fmt = mpl.ticker.ScalarFormatter(useOffset=False)
+    fmt.set_scientific(False)
+    ax.xaxis.set_major_formatter(fmt)
+    ax.set_ylim([0,1500])
+    ax.set_xlim([2001,2015.1])
+    ax.legend()
+    a, b, c, d, e = np.polyfit(date1, sdb, 4)
+    a2, b2, c2, d2, e2 = np.polyfit(date2, sdb2, 4)
+    a3, b3, c3, d3, e3 = np.polyfit(date3, sdb3, 4)
+    date1 = np.array(date1)
+    date2 = np.array(date2)
+    date3 = np.array(date3)
+    plt.plot(date1, a * date1 ** 4 + b * date1 **
+             3 + c * date1 ** 2 + d * date1 + e, color='g')
+    plt.plot(date2, a2 * date2 ** 4 + b2 * date2 **
+             3 + c2 * date2** 2 + d2 * date2+ e2, color='b')
+    plt.plot(date3, a3 * date3** 4 + b3 * date3 **
+             3 + c3 * date3 ** 2 + d3 * date3+ e3, color='k')
+    fig = plt.gcf()
+    fig.set_size_inches(12, 8)
+    fig.savefig('date-sdb', dpi=300)
+    plt.close()
